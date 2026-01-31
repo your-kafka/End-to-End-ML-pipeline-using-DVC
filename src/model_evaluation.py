ed420650542d78cf940a151a0f848e0bc05d3b30
Ramkrishna Rathore <tspy1947@gmail.com>
2026-01-31 18:57:06 +0530

    experiment tracking using dvclive done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,6 +5,9 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
+import yaml
+from dvclive import Live
+
 
 log_dir = 'logs'
 os.makedirs(log_dir, exist_ok=True)
@@ -26,6 +29,24 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
 
 def load_data(file_path:str) -> pd.DataFrame :
     try :
@@ -88,6 +109,7 @@ def save_metrics(metrics : dict,file_path : str) -> None:
 
 def main():
     try:
+        params = load_params(params_path = 'params.yaml')
         clf = load_model('/Users/lucky/End-to-End-ML-pipeline-using-DVC/models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -95,6 +117,14 @@ def main():
         y_test = test_data.iloc[:, -1].values
 
         metrics = evaluate_model(clf, X_test, y_test)
+        
+        #experiment tracking using dvclive
+        with Live(save_dvc_exp=True) as live:
+          live.log_metric('accuracy', accuracy_score(y_test, y_test))
+          live.log_metric('precision', precision_score(y_test, y_test))
+          live.log_metric('recall', recall_score(y_test, y_test))
+          live.log_params(params)
+        
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
